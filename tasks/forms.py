9cdfeb7d11fd28e0a31bfc8582f6230785367a7b
1208kelly <1208kelly@gmail.com>
2020-09-13 16:46:57 +0000

    Added css styling and buttons

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,6 +4,9 @@ from .models import *
 
 
 class TaskForm(forms.ModelForm):
+    title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
+
+
     class Meta:
         model = Task
         fields = '__all__'
