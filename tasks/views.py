a5c22725995b2eaf47cfef56e798ac07ee14d367
1208kelly <1208kelly@gmail.com>
2020-09-13 11:44:36 +0000

    Added ability to update items on form

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -10,5 +10,28 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'tasks/update_task.html', context)
